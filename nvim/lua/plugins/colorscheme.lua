ac013e0b7186b8bd39ad46ccefe6a0eff59685de
jglim <jglim@sellmate.co.kr>
2026-03-20 18:16:07 +0900

    change color scheme to try out

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,32 @@
 return {
-  "navarasu/onedark.nvim",
-  priority = 1000, -- make sure to load this before all the other start plugins
+  "AlexvZyl/nordic.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
-    require("onedark").setup({
-      style = "dark",
-    })
-    require("onedark").load()
+    require("nordic").load()
   end,
 }
+-- return {
+--   "projekt0n/github-nvim-theme",
+--   name = "github-theme",
+--   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require("github-theme").setup({
+--       -- ...
+--     })
+--
+--     vim.cmd("colorscheme github_dark_colorblind")
+--   end,
+-- }
+
+-- return {
+--   "navarasu/onedark.nvim",
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require("onedark").setup({
+--       style = "dark",
+--     })
+--     require("onedark").load()
+--   end,
+-- }
