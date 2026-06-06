16ce74a9ef073711d262252adeecdd30bde5995f
jasonbanboa <jasonbanboa123@gmail.com>
2026-06-06 20:26:08 +0900

    replace snacks file tree with neotree

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,6 +1,16 @@
 return {
   "folke/snacks.nvim",
+  keys = {
+    -- disable snacks explorer keys so neo-tree owns these (see neo-tree.lua)
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+  },
   opts = {
+    explorer = {
+      replace_netrw = false, -- let neo-tree handle `nvim .` instead of snacks explorer
+    },
     picker = {
       sources = {
         files = {
